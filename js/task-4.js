016990e0ebc5a958349e21a7e35ad3a45fabae0c
AllaHanol <ganol2014@gmail.com>
2024-05-01 23:56:43 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -2,7 +2,13 @@
 // перший параметр users — масив об’єктів користувачів,
 // другий параметр gender — рядок, що зберігає стать.
 // Функція має використовувати ланцюжок виклику методів та повертати загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
-
+const getTotalBalanceByGender = (users, gender) =>
+     users
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => {
+      return totalBalance + user.balance;
+    }, 0);   
+    
 const clients = [
 	{
     name: "Moore Hensley",
