c3ecbd0092e18dc2b7a24313203ee58033ffa4d4
AllaHanol <ganol2014@gmail.com>
2024-05-01 22:07:03 +0200

    task2

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,7 @@
 // Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів користувачів. Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
 
 const getUserNames = users => users.map(user => user.name);
+//const getUserNames = users => users.map(user => user.email);
 
 console.log(
   getUserNames([
